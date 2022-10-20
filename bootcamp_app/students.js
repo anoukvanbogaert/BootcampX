53421d69d9317929cae08f8e1bdfcdbd2749ca7f
Anouk Van Bogaert <vb.anouk@gmail.com>
2022-10-20 03:44:01 +0000

    paramterization of students file and teachers file

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,16 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+// parameterization
 const cohort = process.argv[2];
 const limit = process.argv[3];
+const values = [`%${cohort}%`, limit];
 
 pool.query(`
 SELECT students.id as id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(res.rows);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,6 +8,7 @@ const pool = new Pool({
 });
 
 const cohort = process.argv[2];
+const values = [`%${cohort}%`];
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -15,9 +16,9 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohort}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort}: ${user.teacher} `);
